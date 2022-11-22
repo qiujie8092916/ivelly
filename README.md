cfc543fddba9476bfdd317902c378485e8911170
qiujie <qiujieee_empty@outlook.com>
2022-11-22 23:53:41 +0800

    :tada: feature: migrate homePage to umi

---
+++ README.md
@@ -0,0 +1,9 @@
+# build
+
+```shell
+git tag v0.0.1;
+
+python make.py v0.0.1 build
+
+python make.py v0.0.1 docker
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+
+import React from 'react';
+import Github from './components/Github';
+import styles from './index.less';
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <header>
+        <h1 className={clsx(styles.header, styles.center, styles.mt15)}>
+          Hello World
+        </h1>
+      </header>
+
+      <div className={styles.container}>
+        <a className={clsx(styles.mlr20, styles['no-decoration'])} href="/resume">
+          Resume（Invalid）
+        </a>
+        <a className={clsx(styles.mlr20, styles['no-decoration'])} href="/axure">
+          Axure
+        </a>
+        <a className={clsx(styles.mlr20, styles['no-decoration'])} href="//ivelly.com:9001">
+          Supervisor
+        </a>
+        <a
+          className={clsx(styles.mlr20, styles['no-decoration'])}
+          href="//sentry.iskytrip.ivelly.com:8881"
+        >
+          Ngrok
+        </a>
+      </div>
+
+      <footer className={clsx(styles.center, styles.footer)}>
+        <a href="https://beian.miit.gov.cn">蜀ICP备20000809号-1</a>
+      </footer>
+
+      <Github />
+    </>
+  );
+};
+
+export default HomePage;
